47dcd31206cc50cfcbd7c6dc8700c965433aa490
debop <sunghyouk.bae@gmail.com>
2026-07-12 16:37:56 +0900

    feat: compose Redis result envelope codecs

--- packages/bluetape-cache-redis/src/bluetape/cache/redis/__init__.py
+++ packages/bluetape-cache-redis/src/bluetape/cache/redis/__init__.py
@@ -19,6 +19,7 @@ from ._contracts import (
     RedisProviderError,
     ResultEnvelope,
 )
+from ._envelope import ResultEnvelopeCodec
 from ._formats import BinaryEnvelopeFormat, JsonEnvelopeFormat
 
 __all__ = [  # noqa: RUF022 - public order is part of the contract
@@ -41,4 +42,5 @@ __all__ = [  # noqa: RUF022 - public order is part of the contract
     "EnvelopeDecodeError",
     "BinaryEnvelopeFormat",
     "JsonEnvelopeFormat",
+    "ResultEnvelopeCodec",
 ]
